7720a10f3723298c4c05913c0617fb99344a4ba9
ksingletary <keithsingletary@gmail.com>
2024-03-07 11:25:07 -0500

    react jokes exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import JokeListComponent from "./JokeListComponent";
+// import JokeList from "./JokeList";
+// import JokeListComponent from "./JokeListComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/** App component. Renders list of jokes. */
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <JokeListComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
